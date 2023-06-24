54e063cc30d135ac592858174f32a2b50bfff51b
marceliPolak <marcelpolak97@gmail.com>
2023-06-24 07:54:06 +0200

    Initial commit

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę kolejnych liczb pierwszych do wygenerowania: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int count = 0;
+        int number = 2;
+
+        while (count < n)
+        {
+            if (IsPrime(number))
+            {
+                Console.WriteLine(number);
+                count++;
+            }
+
+            number++;
+        }
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2)
+            return false;
+
+        for (int i = 2; i * i <= number; i++)
+        {
+            if (number % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Zadanie3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj wysokość trójkąta: ");
+        int height = Convert.ToInt32(Console.ReadLine());
+
+        DrawTriangle(height);
+    }
+
+    static void DrawTriangle(int height)
+    {
+        int width = 1;
+        int spaces = height - 1;
+
+        for (int i = 0; i < height; i++)
+        {
+            PrintSpaces(spaces);
+            PrintPluses(width);
+
+            Console.WriteLine();
+
+            width += 2;
+            spaces--;
+        }
+    }
+
+    static void PrintSpaces(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(" ");
+        }
+    }
+
+    static void PrintPluses(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("+");
+        }
+    }
+}
